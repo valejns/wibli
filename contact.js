1a354484d0fffc260ca39dcb2b98b6193b054000
Valentina Muratore <valejns@gmail.com>
2022-05-26 18:16:47 -0300

    <3

--- contact.js
+++ contact.js
@@ -1,33 +1,47 @@
 const nameInput= document.querySelector("#name");
 const emailInput= document.querySelector("#email");
 const messageInput= document.querySelector("#message");
-
+const html = document.querySelector('#cartel');
 
 function validateForm(){
     var regexEmail =/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
     var regexName= /^[a-zA-Z]{3,}$/;
 
     let errorNameInput = regexName.test(nameInput.value)?
-    false : '¿Estas seguro de que ingresaste bien el nombre?'
+    null : '¿Estas seguro de que ingresaste bien el nombre?'
 
     let errorEmailInput = (regexEmail.test(emailInput.value)) ?
-    false : '¿Estas seguro de que ingresaste bien el mail?'
+    null : '¿Estas seguro de que ingresaste bien el mail?'
 
     let errorMessageInput = (messageInput.value.length >0) ?
-    false : 'Ingresa un mensaje'
+    null : 'Ingresa un mensaje'
 
-    if(errorNameInput && errorEmailInput && errorMessageInput == false){
-        console.log("q pasa aca");
-        // document.getElementById("#myForm").submit();
+    if(!errorNameInput && !errorEmailInput && !errorMessageInput){
+        document.getElementById("myForm").submit();
     }            
     
-    // else if(errorNameInput && errorEmailInput && errorMessageInput){
-    //     <div class="cartel">
-    //         <div class="error">holi</div>
-    //         <img class="hands" src="./images/hands.svg"/>
-    //     </div>
+    else if(errorNameInput && errorEmailInput && errorMessageInput){
+        html.innerHTML=
+        `<div class="error">mmm... check the fields</div>
+        <img class="hands" src="./images/hands.svg"/>`
+    }
+    else if(errorNameInput){
+        html.innerHTML=
+        `<div class="error">check your name</div>
+        <img class="hands" src="./images/hands.svg"/>`
 
-    // }
+    }
+    else if(errorEmailInput){
+        html.innerHTML=
+        `<div class="error">check your e-mail</div>
+        <img class="hands" src="./images/hands.svg"/>`
+
+    }
+    else if(errorMessageInput){
+            html.innerHTML=
+            `<div class="error">please write a message</div>
+            <img class="hands" src="./images/hands.svg"/>`
 
     }
 
+    }
