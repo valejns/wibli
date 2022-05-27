b5d8999e4e62cf4d1596ef16c5ab51f7d401d9bb
Valentina Muratore <valejns@gmail.com>
2022-05-27 00:54:55 -0300

    <3

--- random.js
+++ random.js
@@ -13,24 +13,27 @@ request.open('GET',`${API_URL}/films`);
 request.send();
 
 function obtenerRandom(){
+    document.getElementById('cabezon').setAttribute('class', 'head-clicked')
+    setTimeout(() => {
+        document.getElementById('cabezon').setAttribute('class', 'head')
+    }, 500);
+    
     const randomMovie = movies[Math.floor(Math.random() * movies.length)];
     const html = document.querySelector('#rdm')
-        html.innerHTML= `<div class="movie">
-        
-        <div class="boxes">
-        <h2 class="movieTitle">${randomMovie.title}</h2>
-        <p class="text">${randomMovie.release_date}</p>
+        html.innerHTML= `
+    <div class="image-cropper">
+        <img class="img" src="${randomMovie.image}" class="rounded" />
+    </div>
+    <div class="movie">
+       
+        <div class="left">
+            <img class="poster" src="${randomMovie.image}"/>
         </div>
-        <div class="boxes">
-        <p class="originalTitle">${randomMovie.original_title}</p>
-        <p class="text">${randomMovie.director}</p>
+        <div class="right">
+            <h2 class="title">${randomMovie.title} <span>(${randomMovie.release_date})</span></h2>
+            <h3 class="title-original">${randomMovie.original_title}<span>${randomMovie.director}</span></h3>
+            <p  class="description">${randomMovie.description}</p>
         </div>
-        
-        <div class="boxes">
-        <img class="poster" src="${randomMovie.image}"/>
-        <p class="description">${randomMovie.description}</p>
-        </div>
-
-        </div>
+    </div>
         `
 }
