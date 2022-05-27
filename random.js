e2994a4f2d6e1174bd849ffd20314cd957693d59
Valentina Muratore <valejns@gmail.com>
2022-05-26 22:06:06 -0300

    <3

--- random.js
+++ random.js
@@ -16,17 +16,20 @@ function obtenerRandom(){
     const randomMovie = movies[Math.floor(Math.random() * movies.length)];
     const html = document.querySelector('#rdm')
         html.innerHTML= `<div class="movie">
-        <div>
-        <h2 class="title">${randomMovie.title}</h2>
+        <div class="boxes">
+        <h2 class="movieTitle">${randomMovie.title}</h2>
         <p class="text">${randomMovie.release_date}</p>
         </div>
-        <div>
+        <div class="boxes">
         <p class="originalTitle">${randomMovie.original_title}</p>
         <p class="text">${randomMovie.director}</p>
         </div>
         
+        <div class="boxes">
         <img class="poster" src="${randomMovie.image}"/>
         <p class="description">${randomMovie.description}</p>
+        </div>
+
         </div>
         `
 }
